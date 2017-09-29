c2123e4aab17317d3fbaaac602e2dd191139ee08
BastyZ <bastian.inostroza@gmail.com>
2017-09-29 00:08:09 -0300

    CalculateMBR overloaded with options to:
    - two Rectangles
    - A rectangle and an Array of Rectangles
    
    functions, and deleted older functions that are deprecated
    
    closes #14

--- src/com/logaritmos/Rectangle.java
+++ src/com/logaritmos/Rectangle.java
@@ -147,33 +147,4 @@ public class Rectangle implements Serializable {
     return calculateMBR(list);
   }
 
-  // --------------------- Old Functions ------------------------------
-
-  public boolean overlaps(Rectangle aRectangle) {
-    return (this.inVertical(aRectangle) && this.inHorizontal(aRectangle));
-  }
-  private boolean inVertical(Rectangle aRectangle){
-   return (this.inTop(aRectangle.top) || this.inBottom(aRectangle.bottom));
-  }
-  private boolean inHorizontal(Rectangle aRectangle){
-    return (this.inLeft(aRectangle.left) || this.inRight(aRectangle.right));
-  }
-  private boolean contains(int number, int min ,int max){
-    return number>=min && number<=max;
-  }
-  private boolean inTop(int aTop){
-    return contains(aTop,this.bottom,this.top);
-  }
-  private boolean inBottom(int aBottom){
-    return contains(aBottom,this.bottom,this.top);
-  }
-  private boolean inLeft(int aLeft){
-    return contains(aLeft,this.left,this.right);
-  }
-  private boolean inRight(int aRight){
-    return contains(aRight,this.left,this.right);
-  }
-
-  public void multiMax(int i, int i1, int i2, int i3) {
-  }
 }
